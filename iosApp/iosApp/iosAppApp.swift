c67e2633a3eff555a66c132f815e8b0a1f0e2607
ghilmankhan <khanghilman96@gmail.com>
2026-06-09 12:21:17 +0500

    iosApp manually created

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by ghilman Islam360 on 6/9/26.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
